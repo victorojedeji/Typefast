7670d0bda598852dbb558442a40ad17dd01f1727
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-04 16:29:13 +0100

    initial commit

---
+++ js/typefast.js
@@ -0,0 +1,87 @@
+const levelOne = [
+    {
+        id: 1,
+        content: "Excrusiating",
+    },
+    {
+        id: 2,
+        content: "believing",
+    },
+  //  {
+      //  id: 3,
+      //  content: "spacious"
+    //},
+    {
+        id: 3,
+        content: "arguments",
+    },
+ //   {
+    //    id: 5,
+     //   content: "nonsense",
+    //},
+    //{
+  //      id: 6,
+   //     content: "palatable"
+  //  },
+  //  {
+  //      id: 7,
+  //      content: "tantalizing"
+ //   },
+    {
+        id: 4,
+        content: "clandestine",
+    },
+  //  {
+  //      id: 9,
+  //      content: "pulsating"
+  //  },
+    {
+        id: 5,
+        content: "treatable",
+    }
+];
+
+const levelTwo = [
+    {
+        id: 1,
+        content: "i typed this really fast",
+    },
+   // {
+   //     id: 2,
+   //     content: "i am on my way out"
+   // },
+   // {
+   //     id: 3,
+   //     content: "you play too much"
+   // }
+    {
+        id: 2,
+        content: "moving around is not good for children",
+    },
+   // {
+   //     id: 5,
+   //     content: "you are saying nonsense about me again",
+   // },
+   // {
+   //     id: 6,
+   //     content: "lovely day ahead today"
+   // },
+    {
+        id: 3,
+        content: "a good day to write some javascript codes",
+    },
+    {
+        id: 4,
+        content: "listening to refreshing music at the moment",
+    },
+   // {
+   //     id: 9,
+   //     content: "watching the news gets me agitated"
+   // },
+    {
+        id: 5,
+        content: "slept through the night because i was tired",
+    }
+];
+
+console.log(levelOne, levelTwo)
