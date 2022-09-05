457cf5d12f7a54e278fa73dccadfb4ba47137960
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-05 16:19:20 +0100

    linking elements

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const displayContent = document.querySelector(".display-content");
+const checkBtn = document.querySelector(".check-btn");
+const inputField = document.querySelector("#input");
+const time = document.querySelector(".time");
+
+
+
+// let oneArr = levelOne.map(obj => obj.content);
+
+checkBtn.addEventListener("click", )
+
+
+displayContent.setAttribute("title", "")
