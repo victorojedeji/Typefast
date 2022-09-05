e2323cd2c5d0fdc9e249d9830e221e05742bb36c
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-09-05 20:10:20 +0100

    quitGame added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const displayContent = document.querySelector(".display-content");
 const checkBtn = document.querySelector(".check-btn");
 const nextBtn = document.querySelector(".next-btn");
+const quitBtn = document.querySelector(".next-btn");
 const inputField = document.querySelector("#input");
 const time = document.querySelector(".time");
 const startBtn = document.querySelector(".start-btn");
@@ -8,19 +9,28 @@ const startBtn = document.querySelector(".start-btn");
 
 
 
-
-
+startBtn.addEventListener("click", startGame);
+nextBtn.addEventListener("click", next);
 checkBtn.addEventListener("click", check);
+quitBtn.addEventListener("click", quitGame);
+
+
 
 function check() {
     console.log("check activated")
 };
 
-nextBtn.addEventListener("click", next);
+
 let increment = 0;
-if(increment === 0) {
-    displayContent.textContent = levelOne[0].content;
-}
+function startGame() {
+    if(increment === 0) {
+        displayContent.textContent = levelOne[0].content;
+    }
+    nextBtn.removeAttribute("disabled");
+    checkBtn.removeAttribute("disabled");
+    quitBtn.removeAttribute("disabled");
+};
+
 function next() {
     increment = increment + 1;
     if (increment >= 5) {
@@ -42,3 +52,8 @@ function next() {
 };
 
 displayContent.setAttribute("title", "")
+
+
+function quitGame() {
+    console.log("end of the game")
+}
